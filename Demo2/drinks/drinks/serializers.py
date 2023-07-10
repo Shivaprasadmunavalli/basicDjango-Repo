8e350a270b8cfacb97e4c7a36a8b62591bc8e87e
Shivaprasadmunavalli <shivaprasadmunavalli@gmail.com>
2023-07-10 09:40:45 +0530

    error file

---
+++ Demo2/drinks/drinks/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import  serializers
+
+from.models import Drink
+
+
+class DrinkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Drink
+        fields="__all__"
+
+
+# class DrinkSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model=Drink
+#         fields="__all__"
+
